bd3122d9f24ab43c19a0201ce7dc3f55e43ae448
Rao-Owais <rao.owais2020@gmail.com>
2024-05-05 12:52:36 +0500

    updated

--- object.js
+++ object.js
@@ -2,107 +2,128 @@ const products = [
     {
         name: "Laptop",
         price: 999.99,
-        description: "Powerful laptop for all your computing needs."
+        description: "Powerful laptop for all your computing needs.",
+        images : "https://images.unsplash.com/photo-1491472253230-a044054ca35f?q=80&w=1784&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
         name: "Headphones",
         price: 79.99,
-        description: "High-quality headphones for immersive audio experience."
+        description: "High-quality headphones for immersive audio experience.",
+        images: "https://images.unsplash.com/photo-1484704849700-f032a568e944?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
         name: "Smartphone",
         price: 699.99,
-        description: "Advanced smartphone with cutting-edge features."
-    },
+        description: "Advanced smartphone with cutting-edge features.",
+        images: "https://images.unsplash.com/photo-1572766156884-828770df8d7e?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+},
     {
         name: "Tablet",
         price: 449.99,
-        description: "Portable tablet for entertainment and productivity."
-    },
+        description: "Portable tablet for entertainment and productivity.",
+        images: "https://images.unsplash.com/photo-1604399852419-f67ee7d5f2ef?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+},
     {
         name: "Smartwatch",
         price: 249.99,
-        description: "Stay connected and track your fitness with this smartwatch."
-    },
+        description: "Stay connected and track your fitness with this smartwatch.",
+        images: "https://images.unsplash.com/photo-1484704849700-f032a568e944?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+},
     {
         name: "Gaming Console",
         price: 399.99,
-        description: "Experience the thrill of gaming with this powerful console."
-    },
+        description: "Experience the thrill of gaming with this powerful console.",
+        images: "https://images.unsplash.com/photo-1484704849700-f032a568e944?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+},
     {
         name: "Camera",
         price: 599.99,
-        description: "Capture life's moments with stunning clarity and detail."
-    },
+        description: "Capture life's moments with stunning clarity and detail.",
+        images: "https://images.unsplash.com/photo-1484704849700-f032a568e944?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+},
     {
         name: "Wireless Speaker",
         price: 129.99,
-        description: "Enjoy your favorite music wirelessly with this speaker."
-    },
+        description: "Enjoy your favorite music wirelessly with this speaker.",
+        images: "https://images.unsplash.com/photo-1484704849700-f032a568e944?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+},
     {
         name: "External Hard Drive",
         price: 129.99,
-        description: "Expand your storage and backup your files with ease."
-    },
+        description: "Expand your storage and backup your files with ease.",
+        images: "https://images.unsplash.com/photo-1484704849700-f032a568e944?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+},
     {
         name: "Wireless Mouse",
         price: 29.99,
-        description: "Navigate your computer with precision and comfort."
-    },
+        description: "Navigate your computer with precision and comfort.",
+        images: "https://images.unsplash.com/photo-1484704849700-f032a568e944?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+},
     {
         name: "Printer",
         price: 199.99,
-        description: "Print documents and photos with exceptional quality."
-    },
+        description: "Print documents and photos with exceptional quality.",
+        images: "https://images.unsplash.com/photo-1484704849700-f032a568e944?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+},
     {
         name: "Desk Lamp",
         price: 49.99,
-        description: "Illuminate your workspace with adjustable brightness."
-    },
+        description: "Illuminate your workspace with adjustable brightness.",
+        images: "https://images.unsplash.com/photo-1484704849700-f032a568e944?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+},
     {
         name: "Fitness Tracker",
         price: 79.99,
-        description: "Monitor your health and track your workouts."
-    },
+        description: "Monitor your health and track your workouts.",
+        images: "https://images.unsplash.com/photo-1484704849700-f032a568e944?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+},
     {
         name: "Bluetooth Earbuds",
         price: 99.99,
-        description: "Wireless earbuds for on-the-go audio enjoyment."
-    },
+        description: "Wireless earbuds for on-the-go audio enjoyment.",
+        images: "https://images.unsplash.com/photo-1484704849700-f032a568e944?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+},
     {
         name: "Portable Charger",
         price: 39.99,
-        description: "Charge your devices anywhere, anytime."
+        description: "Charge your devices anywhere, anytime.",
+        images: "https://images.unsplash.com/photo-1484704849700-f032a568e944?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
         name: "USB Flash Drive",
         price: 19.99,
-        description: "Store and transfer your files conveniently."
+        description: "Store and transfer your files conveniently.",
+        images: "https://images.unsplash.com/photo-1484704849700-f032a568e944?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
         name: "Monitor",
         price: 299.99,
-        description: "Upgrade your display for enhanced productivity."
+        description: "Upgrade your display for enhanced productivity.",
+        images: "https://images.unsplash.com/photo-1484704849700-f032a568e944?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
         name: "Keyboard",
         price: 49.99,
-        description: "Type comfortably with this reliable keyboard."
+        description: "Type comfortably with this reliable keyboard.",
+        images: "https://images.unsplash.com/photo-1484704849700-f032a568e944?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
         name: "Computer Mousepad",
         price: 9.99,
-        description: "Enhance your mouse precision with this smooth pad."
+        description: "Enhance your mouse precision with this smooth pad.",
+        images: "https://images.unsplash.com/photo-1484704849700-f032a568e944?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
         name: "Graphic Tablet",
         price: 199.99,
-        description: "Express your creativity with this digital drawing tablet."
+        description: "Express your creativity with this digital drawing tablet.",
+        images: "https://images.unsplash.com/photo-1484704849700-f032a568e944?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
         name: "Wireless Router",
         price: 79.99,
-        description: "Fast and reliable internet connection for your home network."
+        description: "Fast and reliable internet connection for your home network.",
+        images: "https://images.unsplash.com/photo-1484704849700-f032a568e944?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     }
 ];
 
@@ -111,9 +132,19 @@ const div = document.querySelector('div');
 for (let i = 0; i < products.length; i++) {
     
     div.innerHTML += `
-        <h1>NAME : ${products[i].name}</h1>
-        <h2>PRICE : ${products[i].price}</h2>
-        <p>${products[i].description}</p>
-        <hr/>
+    <div class="card" style="width: 18rem;">
+    <img src="${products[i].images}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${products[i].name}</h5>
+      <h6 class="card-title">Price : ${products[i].price}</h6>
+      <p class="card-text">${products[i].description}</p>
+      <a href="#" onclick="display(${i})" class="btn btn-primary">Add To Cart</a>
+    </div>
+  </div>
     `;
+}
+
+function display(index){
+    console.log(products[index]);
+        
 }
